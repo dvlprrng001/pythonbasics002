de327233123acd52a00d54316541aee2697944a5
dvlprrng001 <105548018+dvlprrng001@users.noreply.github.com>
2022-05-14 18:38:25 +0530

    Update Test.py
    This is a trial class

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-print("hello world.. this is a test")
+class Employee:
+    def __init__(self, name, date_of_birth) -> None:
+        self.name = name
+        self.date_of_birth = date_of_birth
+    
+    def Info(self):
+        print(f''' Hello {self.name}  .. i am {self.date_of_birth} years old''')
+    
+emp01 = Employee("Shravya","10/10/2010")
+emp01.Info()
